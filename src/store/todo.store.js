91a2f6a1a34afdd3d52d141fdff4980f98841b8b
Christian Saavedra <chrisosami86@gmail.com>
2024-04-26 09:58:59 -0500

    Update Store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -9,19 +9,82 @@ const Filters = {
 
 const state = {
     todos: [
-        new Todo ( 'Piedra del Tiempo' ),
-        new Todo ( 'Piedra del Alma' ),
-        new Todo ( 'Piedra del Vida' ),
+        new Todo('Piedra del Tiempo'),
+        new Todo('Piedra del Alma'),
+        new Todo('Piedra del Vida'),
     ],
 
     filter: Filters.All
 }
 
-const initStore = () =>{
+const initStore = () => {
     console.log(state);
     console.log('InitStore 🥑');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+const getTodos = ( filter = Filters.All) => {
+    switch ( filter ) {
+        case Filters.All:
+            return [...state.todos];
+        
+        case Filters.Completed:
+            return state.todos.filter((todo) => todo.done);
+        
+        case Filters.Pending:
+            return state.todos.filter( (todo) => !todo.done);
+        
+        default:
+            throw new Error(`Option ${ filter } is not valid`);
+    }
+}
+
+const addTodo = (description) => {
+    if (!description) throw new Error('Description is required');
+
+    state.todos.push(new Todo(description));
+}
+
+const toggleTodo = (todoId) => {
+    state.todos = state.todos.map( (todo) => {
+        if (todo.id === todoId){
+            todo.done = !todo.done;
+        }
+        return todo;
+    }); 
+}
+
+const deleteTodo = (todoId) => {
+    state.todos = state.todos.filter( (todo) => todo.id !== todoId);
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter( (todo) => !todo.done)
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = (newFilter = Filters.All) => {
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo
 }
