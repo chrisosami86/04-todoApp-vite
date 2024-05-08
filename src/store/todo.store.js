d1669b2d3a5a73566b495d230678f58fd3e1c268
Christian Saavedra <chrisosami86@gmail.com>
2024-05-08 11:33:46 -0500

    Implemented selected filter

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,7 +1,7 @@
 import { Todo } from '../todos/models/todo.model';
 
 
-const Filters = {
+export const Filters = {
     All: 'all',
     Completed: 'Completed',
     Pending: 'Pending'
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,5 +1,5 @@
 import html from './app.html?raw'; //Importacion en crudo de html
-import todoStore from '../store/todo.store';
+import todoStore, { Filters } from '../store/todo.store';
 import { renderTodos } from './use-cases';
 
 
@@ -7,6 +7,7 @@ const elementIDs = {
     ClearCompleted: '.clear-completed',
     TodoList: '.todo-list',
     NewTodoInput: '#new-todo-input',
+    TodoFilters: '.filtro',
 }
 
 /**
@@ -33,6 +34,7 @@ export const App = (elementId) => {
     const newDescriptionInput = document.querySelector(elementIDs.NewTodoInput);
     const todoListUl = document.querySelector(elementIDs.TodoList);
     const btnClearCompleted = document.querySelector(elementIDs.ClearCompleted);
+    const filtersLIs = document.querySelectorAll(elementIDs.TodoFilters);
 
     //Listener
     newDescriptionInput.addEventListener('keyup', ( event ) => {
@@ -67,5 +69,32 @@ export const App = (elementId) => {
         todoStore.deleteCompleted();
         displayTodos();
     });
+
+    filtersLIs.forEach( element  => {
+        element.addEventListener('click', ( element )=>{
+            filtersLIs.forEach( el => { el.classList.remove('selected')} );
+            element.target.classList.add('selected');
+
+            switch (element.target.text){
+                case 'Todos':
+                    todoStore.setFilter(Filters.All);                    
+                break;
+
+                case 'Pendientes':
+                    todoStore.setFilter(Filters.Pending);                 
+                break;
+
+                case 'Completados':
+                    todoStore.setFilter(Filters.Completed); 
+                break;
+
+                default:
+                    throw new Error('Filter is not Valid');            
+            }
+
+            displayTodos();
+
+        });
+    } );
 
 }
